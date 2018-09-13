5f0c44bba453f0849cd5a2268ef4dcda22343de9
arekoleksik <arkadiuszoleksik@gmail.com>
2018-09-13 17:39:51 +0200

    two basic views

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -11,7 +11,7 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import './shared-styles.js';
 
-class MyView2 extends PolymerElement {
+class ShoppingList extends PolymerElement {
   static get template() {
     return html`
       <style include="shared-styles">
@@ -23,13 +23,13 @@ class MyView2 extends PolymerElement {
       </style>
 
       <div class="card">
-        <div class="circle">2</div>
-        <h1>View Two</h1>
-        <p>Ea duis bonorum nec, falli paulo aliquid ei eum.</p>
-        <p>Id nam odio natum malorum, tibique copiosae expetenda mel ea.Detracto suavitate repudiandae no eum. Id adhuc minim soluta nam.Id nam odio natum malorum, tibique copiosae expetenda mel ea.</p>
+        <div class="circle">1</div>
+        <h1>View One</h1>
+        <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
+        <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
       </div>
     `;
   }
 }
 
-window.customElements.define('my-view2', MyView2);
+window.customElements.define('shopping-list', ShoppingList);
